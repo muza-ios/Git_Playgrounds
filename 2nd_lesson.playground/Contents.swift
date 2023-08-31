05f355acab1ce024ab66d0630b3c12e318e03c7e
Muzaffar <muza.mamatmurodov@gmail.com>
2023-08-31 22:16:41 +0500

    git_push

---
+++ 2nd_lesson.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// Types
+
+var x = 3  // integer
+var name = "Muzaffar"  // string
+
+var lastName = "Mamatmurodov"
+
+let age: Int = 20
+let price: Double = 199.990
+var height: Float = 180.348759
+
+class Person{
+    
+}
+
+var Mustafo: Person = Person()
+
+var button1: UIButton = UIButton()
+
+var isComplete: Bool = false
+isComplete = true
+
+
+
---
+++ Various& Constants.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var str = "Hello, World!"
+var x = 4
+var y = 2
+
+y = 5
+
+var z = "Will"
+
+class Person {
+    static let age = 45
+    let lastName = "Smith"
+}
+
+var john = Person()
+var jack = Person()
+var Tim = Person()
+var Dave = Person()
+
+let Dwayne = Person.age
+
+let price = 1.10
+var name = "Joe"
+name = "Jill"
+
+
+
