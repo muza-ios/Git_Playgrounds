26ec30d36c068ec37dbabc77251edb0bd6fe68f1
Muzaffar <muza.mamatmurodov@gmail.com>
2023-09-07 15:41:45 +0500

    OptionalUnwrapping

---
+++ 6th_lesson.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// Optoinal&Unwrapping
+
+/* var name: String? = "Muzaffar"
+
+name = nil */
+
+var x: Int? = 3
+var y: Int? = 12
+
+/* if let value = x {
+    if let value2 = y {
+        let result = value + value2
+        print(result)
+    }
+} */
+
+var price: Double? = nil
+
+print(price)
+price = 1.5
+print(price)
+
+type(of: price)
