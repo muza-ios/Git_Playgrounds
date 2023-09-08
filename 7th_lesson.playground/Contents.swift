327f6d8cac8753b60c4ec909448b7657e0f5cbe4
Muzaffar <muza.mamatmurodov@gmail.com>
2023-09-09 00:06:04 +0500

    Playground

---
+++ 7th_lesson.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// If Else Conditionals
+
+var x = 1
+var y = 12
+
+// we can use also "||" means "or" in conditionals
+
+if x > 10 && x < 200 {
+    print(x+y)
+}
+else {
+    print(x*y)
+}
